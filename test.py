8ea270a960329b141b493ea1e13820da3e2f0e99
raptor72 <mctaras51@yandex.ru>
2020-02-27 06:02:54 +0000

    Add load_store and context fixtures

--- test.py
+++ test.py
@@ -5,6 +5,16 @@ from collections import namedtuple
 from store import Store
 
 
+@pytest.fixture
+def load_store():
+    s = Store({"46a15aeae88d2123e8ac038602ee248f": 34, "1": 1, "2": "pets"})
+    return s
+
+@pytest.fixture
+def context():
+    return {}
+
+
 def get_response(request, headers, context, store):
     return api.method_handler({"body": request, "headers": headers}, context, store)
 
@@ -18,7 +28,6 @@ def set_valid_auth(request):
     return request["token"]
 
 
-
 @pytest.mark.parametrize("arguments", [{"phone": "79175002040", "email": "stupnikov@otus.ru"},
                                        {"gender": 1, "birthday": "01.01.2000", "first_name": "a", "last_name": "b"},
                                        {"gender": 0, "birthday": "01.01.2000"},
@@ -28,12 +37,10 @@ def set_valid_auth(request):
                          "first_name": "a", "last_name": "b"},
                                        {"phone": 79175002040, "email": "stupnikov@otus.ru"},
 ])
-def test_ok_score_request(arguments):
-    context = {}
+def test_ok_score_request(arguments, load_store, context):
     request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
     set_valid_auth(request)
-    s = Store({"46a15aeae88d2123e8ac038602ee248f": 34, "1": 1, "2": "pets"})
-    response, code = get_response(request, {}, context, s)
+    response, code = get_response(request, {}, context, load_store)
 #    print(api.OK) # 200
 #    print(code) # 200
 #    print(arguments) # {'phone': '79175002040', 'email': 'stupnikov@otus.ru'}
@@ -52,6 +59,15 @@ def test_empty_request():
     _, code = get_response({}, {}, {}, None)
     assert api.INVALID_REQUEST == code
 
+
+@pytest.mark.parametrize("request", [
+        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "", "arguments": {}},
+        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "sdd", "arguments": {}},
+        {"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}},
+])
+def test_bad_auth(request, load_store):
+    _, code = get_response(request, {}, {}, load_store)
+    assert api.FORBIDDEN == code
 
 
 
