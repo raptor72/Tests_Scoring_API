9af2301259c0c8b838bdcc7c0d72bfe76b84da3c
raptor72 <mctaras51@yandex.ru>
2020-02-27 03:59:01 +0000

    correct pytests

--- test.py
+++ test.py
@@ -4,53 +4,46 @@ import hashlib
 from collections import namedtuple
 from store import Store
 
-@pytest.fixture
-def setUp():
-    context = {}
-    headers = {}
-    store = Store
-#    store = None
 
-#    return {"context": context, "headers": headers, "store": Store}
-
-print(setUp.context)
+def get_response(request, headers, context, store):
+    return api.method_handler({"body": request, "headers": headers}, context, store)
 
 
-
-def get_response(setUp, request):
-    return api.method_handler({"body": request, "headers": setUp["headers"]}, setUp["context"], setUp["store"])
-
-
-def set_valid_auth(setUp, request):
+def set_valid_auth(request):
     if request.get("login") == api.ADMIN_LOGIN:
         request["token"] = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT).hexdigest()
     else:
         msg = request.get("account", "") + request.get("login", "") + api.SALT
         request["token"] = hashlib.sha512(msg).hexdigest()
+    return request["token"]
 
-#    return request["token"]
 
-def test_empty_request(setUp):
-    _, code = get_response(setUp, {})
+def test_ok_score_request(arguments={"phone": "79175002040", "email": "stupnikov@otus.ru"}):
+    context = {}
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+    set_valid_auth(request)
+    s = Store({"46a15aeae88d2123e8ac038602ee248f": 34, "1": 1, "2": "pets"})
+    response, code = get_response(request, {}, context, s)
+#    print(api.OK) # 200
+#    print(code) # 200
+#    print(arguments) # {'phone': '79175002040', 'email': 'stupnikov@otus.ru'}
+#    print("response", response) #('response', {'score': 3.0})
+#    print("context", context) # ('context', {'has': ['phone', 'email']})
+    assert api.OK == code
+    score = response.get("score")
+#    print(score) # 3.0
+    assert isinstance(score, int) or isinstance(score, float)
+    assert score >= 0
+    assert sorted(context["has"]) == sorted(arguments.keys())
+
+
+def test_empty_request():
+    s = Store({"46a15aeae88d2123e8ac038602ee248f": 34, "1": 1, "2": "pets"})
+    _, code = get_response({}, {}, {}, None)
     assert api.INVALID_REQUEST == code
 
 
 
-#def test_ok_score_request(setUp, arguments={"phone": "79175002040", "email": "stupnikov@otus.ru"}):
-#    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
-#    set_valid_auth(setUp, request)
-#    response, code = get_response(setUp, request)
-#    return response, code
-#    assert api.OK == code
-#    assert code == arguments
-#    score = response.get("score")
-#    self.assertTrue(isinstance(score, (int, float)) and score >= 0, arguments)
-#    self.assertEqual(sorted(self.context["has"]), sorted(arguments.keys()))
 
 
-
-#test_ok_score_request(setUp, arguments={"phone": "79175002040", "email": "stupnikov@otus.ru"})
-
-
-#print(get_response(setUp, {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": {"phone": "79175002040", "email": "stupnikov@otus.ru"}}))
 
