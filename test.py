66093e0db0fa40fa94dd7685c8ab1b6d8222abb3
raptor72 <mctaras51@yandex.ru>
2020-03-10 19:01:10 +0000

    Add more tests

--- test.py
+++ test.py
@@ -180,11 +180,12 @@ def test_none_store_interests_request(arguments, context, none_store):
     assert 'object has no attribute' in str(e)
 
 
+
 @pytest.fixture
 def load_warm_store():
     s = Store()
     ttl = 20
-    for k, v in ('d41d8cd98f00b204e9800998ecf8427e', 3.0), ('efd4d906526f3a338d9eab83ea4c77e6', 2.0), ('a1301e1514843ca1973b941a88a58092', 0), ('a1301e1514843ca1973b941a88a58092', 1.5), ('187ef4436122d1cc2f40dc2b92f0eba0', 0.5), ('efd4d906526f3a338d9eab83ea4c77e6', 5.0), ('d41d8cd98f00b204e9800998ecf8427e', 3.0), ("1", 1), ("2", "pets"), ("3", "heavy metall"):
+    for k, v in ('d41d8cd98f00b204e9800998ecf8427e', 3.0), ('efd4d906526f3a338d9eab83ea4c77e6', 2.0), ('a1301e1514843ca1973b941a88a58092', 0), ('a1301e1514843ca1973b941a88a58092', 1.5), ('187ef4436122d1cc2f40dc2b92f0eba0', 0.5), ('efd4d906526f3a338d9eab83ea4c77e6', 5.0), ('d41d8cd98f00b204e9800998ecf8427e', 3.0), ("1", 1), ("2", "2"), ("3", "3"), ("-1", "-1"):
         s.cache_set(k, v, ttl)
     return s
 
@@ -293,6 +294,73 @@ def test_too_old_birthday_type_field(arguments, load_warm_store, context):
     response, code = get_response(request, {}, context, load_warm_store)
     assert code == 422
     assert 'Field birthday (type BirthDayField) invalid: Incorrect birth day' in response
+
+
+@pytest.mark.parametrize("arguments", [{"gender": -1, "phone": 79175002040, "email": "stupnikov@otus.ru", "birthday": "01.01.2000", "first_name": "a"},
+                                       {"gender": 3, "phone": 79175002040, "email": "stupnikov@otus.ru", "birthday": "01.01.2000", "first_name": "a"},
+                                       {"gender": "1", "phone": 79175002040, "email": "stupnikov@otus.ru", "birthday": "01.01.2000", "first_name": "a"},
+])
+def test_phone_type_field(arguments, load_warm_store, context):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+    set_valid_auth(request)
+    response, code = get_response(request, {}, context, load_warm_store)
+    assert code == 422
+    assert 'Field gender (type GenderField) invalid: Gender must be equal to 0, 1 or 2' in response
+
+
+@pytest.mark.parametrize("arguments", [
+        {"client_ids": [1, "2", 3], "date": datetime.datetime.today().strftime("%d.%m.%Y")},
+        {"client_ids": [1, -1], "date": "19.07.2017"},
+    ])
+def test_clients_ids_field(arguments, context, load_warm_store):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
+    set_valid_auth(request)
+    response, code = get_response(request, {}, context, load_warm_store)
+    assert code == 422
+    assert 'Client IDs should be list or positive integers' in response
+
+
+@pytest.mark.parametrize("arguments", [[],
+                                       1,
+                                       "phone",
+])
+def test_arguments_field(arguments, load_warm_store, context):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+    set_valid_auth(request)
+    response, code = get_response(request, {}, context, load_warm_store)
+    assert code == 422
+    assert 'Field arguments (type ArgumentsField) invalid: This field must be a dict' in response
+
+
+
+
+@pytest.mark.parametrize("arguments", [{"first_name": ["a"], "phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "last_name": "b"},
+                                       {"first_name": 1, "phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "last_name": "b"},
+                                       {"first_name": 1.0, "phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "last_name": "b"},
+                                       {"first_name": {"k":"v"}, "phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "last_name": "b"},
+])
+def test_char_field(arguments, load_warm_store, context):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+    set_valid_auth(request)
+    response, code = get_response(request, {}, context, load_warm_store)
+    assert code == 422
+    assert '(type CharField) invalid: This field must be a string' in response
+
+
+@pytest.mark.parametrize("arguments", [{"phone": 79175002040, "gender": 1, "first_name": "a"},
+                                       {"email": "stupnikov@otus.ru", "birthday": "01.01.2000", "first_name": "a"},
+                                       {"phone": 79175002040,},
+                                       {"gender": 1, "first_name": "a"},
+                                       {"email": "stupnikov@otus.ru", "last_name": "b"},
+                                       {"phone": 79175002040, "birthday": "01.01.2000", "last_name": "b"},
+                                       {"email": "stupnikov@otus.ru", "gender": 1},
+])
+def test_required_pairs_field(arguments, load_warm_store, context):
+    request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+    set_valid_auth(request)
+    response, code = get_response(request, {}, context, load_warm_store)
+    assert code == 422
+    assert 'At least one of the pairs should be defined: first/last name, email/phone, birthday/gender' in response
 
 
 
